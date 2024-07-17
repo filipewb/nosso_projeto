0051b8410cfefc72e9be82a0ea037e443c19d3b5
Filipe Wolf Boeck <filipe.boeck@hotmail.com>
2024-07-16 22:28:49 -0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,77 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tarefas'),
+        ),
+        body: ListView(
+          children: [
+            Task(
+              nome:
+                  'Aprender Flutter vai ser muito bom pra gente ver olha so isso',
+            ),
+            Task(
+              nome: 'Andar de bike',
+            ),
+            Task(
+              nome: 'Jogar bola',
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nome;
+
+  const Task({super.key, required this.nome});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.black26,
+                    width: 72,
+                    height: 100,
+                  ),
+                  Container(
+                    width: 200,
+                    child: Text(
+                      nome,
+                      style: TextStyle(
+                        fontSize: 24,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Icon(Icons.arrow_drop_up),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
